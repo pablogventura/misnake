a1a67c4e033f06a103ced7109a1435a3ab22f106
Pablo Ventura <pablogventura@gmail.com>
2021-11-18 07:49:43 -0300

    tutorial de pygame

---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+from pygame.locals import *
+ 
+pygame.init()
+vec = pygame.math.Vector2  # 2 for two dimensional
+ 
+HEIGHT = 480
+WIDTH = 640
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+ 
+FramePerSec = pygame.time.Clock()
+ 
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128,255,40))
+        self.rect = self.surf.get_rect(center = (10, 420))
+ 
+class platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, 20))
+        self.surf.fill((255,0,0))
+        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
+ 
+PT1 = platform()
+P1 = Player()
+
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(PT1)
+all_sprites.add(P1)
+ 
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+     
+    displaysurface.fill((0,0,0))
+ 
+    for entity in all_sprites:
+        displaysurface.blit(entity.surf, entity.rect)
+ 
+    pygame.display.update()
+    FramePerSec.tick(FPS)
